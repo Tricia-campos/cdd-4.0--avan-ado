e45c7bf1d00fb0bee314abe298e6c4714b28f730
tricia <triciacamposm@gmail.com>
2024-10-07 11:54:20 -0300

    concluido

--- aula03atvd04.py
+++ aula03atvd04.py
@@ -2,8 +2,11 @@ from biblioteca import*
 
 txt=input("Digite um texto:")
 cont = 0
+tam = len(txt)
 for x in txt:
     if x in "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz":
         cont += 1
 print(f"O texto tem {cont} letras")
 
+for i in range(tam-1,-1,-1):
+    print(txt[i],end=" ")
